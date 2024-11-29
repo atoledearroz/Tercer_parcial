03254746a7682a9871a33754c83e85029371ced8
atoledearroz <ximena.12.gutierrez@gmail.com>
2024-11-28 20:19:45 -0600

    actu
    ya casi

--- Assets/Script/Casitas.cs
+++ Assets/Script/Casitas.cs
@@ -3,19 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Casitas : MonoBehaviour
-{
-    public GameObject casitaIxtab;
-
-    private void OnEnable()
-    {
-        casitaIxtab.SetActive(true);
-    }
-
-    private void OnDisable()
-    {
-        casitaIxtab.SetActive(false);
-    }
-
+{ 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
